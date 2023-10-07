cfe187dfcd38c951a5307a7dd282597d38308de4
Maksuma <max.bakurin@gmail.com>
2023-10-07 19:06:45 +0400

    Edit create/category Add show img after choose

--- static/scripts/create_category.js
+++ static/scripts/create_category.js
@@ -27,3 +27,24 @@ const fileChosen = document.getElementById('file-chosen');
 fileInput.addEventListener('change', function () {
     fileChosen.textContent = this.files[0].name
 })
+
+
+
+// show img
+const droptitle = document.querySelector('.drop-title');
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#show_img')
+                .attr('src', e.target.result)
+                .width(150)
+                .height(200);
+        };
+        fileChosen.classList.add('black');
+        droptitle.classList.add('black');
+        reader.readAsDataURL(input.files[0]);
+    }
+}
