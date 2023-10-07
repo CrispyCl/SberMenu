28cabd80cbc4f3a6dd40bcf6f23006ff93d99a5b
Gogasmerter <chebunin19@gmail.com>
2023-10-07 17:25:32 +0400

    Add session["message"]

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
+from json import dumps, loads
+
 from PIL import Image
 from data import db_session
 from data.categories import Category
 from data.dishes import Dish
 from data.users import User
-from flask import Flask, abort, redirect, render_template
+from flask import Flask, abort, redirect, render_template, session
 from flask_login import LoginManager, current_user, login_required, login_user, logout_user
 from forms.category import CategoryForm
 from forms.dish import DishForm
@@ -13,6 +15,7 @@ from forms.user import UserForm
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "very_secret_key"
+ST_message = {"status": 404, "text": ""}
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -20,13 +23,79 @@ login_manager.init_app(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if not session.get("message"):
+        session["message"] = dumps(ST_message)
+    smessage = session["message"]
+    session["message"] = dumps(ST_message)
+    return render_template("index.html", message=smessage)
+
+
+@app.route("/create_dish", methods=["GET", "POST"])
+def create_dish():
+    if current_user.is_authenticated:
+        abort(404)
+    smessage = session["message"]
+    session["message"] = dumps(ST_message)
+    form = DishForm()
+
+    title = "Создайте блюдо"
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        if db_sess.query(Dish).filter(Dish.title == form.title.data).first():
+            message = {"status": 0, "text": "Такое блюдо уже есть в меню"}
+            return render_template("register_user.html", title=title, form=form, message=message)
+        dish = Dish(title=form.title.data, price=form.price.data, description=form.description.data)
+
+        dishes = db_sess.query(Dish).all()
+        last_id = 1 if not dishes else dishes[-1].id + 1
+        if form.image.data:
+            img1 = form.image.data
+            img1.save(f"static/img/dishes/{last_id}.jpg")
+        else:
+            Image.open("static/img/dishes/no-img.jpg").save(f"static/img/dishes/{last_id}.jpg")
+        dish.image = f"img/dishes/{last_id}.jpg"
+        db_sess.add(dish)
+        db_sess.commit()
+        return redirect("/")
+    return render_template("create_dish.html", title=title, form=form, message=smessage)
+
+
+@app.route("/create_category", methods=["GET", "POST"])
+def create_category():
+    if current_user.is_authenticated:
+        abort(404)
+    smessage = session["message"]
+    session["message"] = dumps(ST_message)
+    form = CategoryForm()
+
+    title = "Создайте категорию"
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        if db_sess.query(Category).filter(Category.title == form.title.data).first():
+            message = {"status": 0, "text": "Такая категория уже есть"}
+            return render_template("register_user.html", title=title, form=form, message=message)
+        category = Category(title=form.title.data)
+
+        categories = db_sess.query(Category).all()
+        last_id = 1 if not categories else categories[-1].id + 1
+        if form.image.data:
+            img1 = form.image.data
+            img1.save(f"static/img/categories/{last_id}.jpg")
+        else:
+            Image.open("static/img/categories/no-img.jpg").save(f"static/img/categories/{last_id}.jpg")
+        category.image = f"img/categories/{last_id}.jpg"
+        db_sess.add(category)
+        db_sess.commit()
+        return redirect("/")
+    return render_template("create_category.html", title=title, form=form, message=smessage)
 
 
 @app.route("/register/user", methods=["GET", "POST"])
 def register_user():
     if current_user.is_authenticated:
         abort(404)
+    smessage = session["message"]
+    session["message"] = dumps(ST_message)
     form = UserForm()
     title = "Регистрация"
     if form.validate_on_submit():
@@ -47,41 +116,15 @@ def register_user():
         db_sess.add(user)
         db_sess.commit()
         return redirect("/")
-    return render_template("register_user.html", title=title, form=form, message="")
-
-
-@app.route("/create_dish", methods=["GET", "POST"])
-def create_dish():
-    if current_user.is_authenticated:
-        abort(404)
-    form = DishForm()
-
-    title = "Новое блюдо"
-    if form.validate_on_submit():
-        db_sess = db_session.create_session()
-        if db_sess.query(Dish).filter(Dish.title == form.title.data).first():
-            message = {"status": 0, "text": "Такое блюдо уже есть в меню"}
-            return render_template("register_user.html", title=title, form=form, message=message)
-        dish = Dish(title=form.title.data, price=form.price.data, description=form.description.data)
-
-        dishes = db_sess.query(Dish).all()
-        last_id = 1 if not dishes else dishes[-1].id + 1
-        if form.image.data:
-            img1 = form.image.data
-            img1.save(f"static/img/dishes/{last_id}.jpg")
-        else:
-            Image.open("static/img/dishes/no-img.jpg").save(f"static/img/dishes/{last_id}.jpg")
-        dish.image = f"img/dishes/{last_id}.jpg"
-        db_sess.add(dish)
-        db_sess.commit()
-        return redirect("/")
-    return render_template("create_dish.html", title=title, form=form, message="")
+    return render_template("register_user.html", title=title, form=form, message=smessage)
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
         return redirect("/")
+    smessage = session["message"]
+    session["message"] = dumps(ST_message)
     title = "Вход"
     form = LoginForm()
     if form.validate_on_submit():
@@ -92,7 +135,7 @@ def login():
             return redirect("/")
         message = {"status": 0, "text": "Неверный логин или пароль"}
         return render_template("login.html", title=title, form=form, message=message)
-    return render_template("login.html", title=title, form=form, message="")
+    return render_template("login.html", title=title, form=form, message=smessage)
 
 
 @login_manager.user_loader
@@ -106,34 +149,6 @@ def load_user(user_id):
 def logout():
     logout_user()
     return redirect("/")
-
-
-@app.route("/create_category", methods=["GET", "POST"])
-def create_category():
-    if current_user.is_authenticated:
-        abort(404)
-    form = CategoryForm()
-
-    title = "Новая категория"
-    if form.validate_on_submit():
-        db_sess = db_session.create_session()
-        if db_sess.query(Category).filter(Category.title == form.title.data).first():
-            message = {"status": 0, "text": "Такая категория уже есть"}
-            return render_template("register_user.html", title=title, form=form, message=message)
-        category = Category(title=form.title.data)
-
-        categories = db_sess.query(Category).all()
-        last_id = 1 if not categories else categories[-1].id + 1
-        if form.image.data:
-            img1 = form.image.data
-            img1.save(f"static/img/categories/{last_id}.jpg")
-        else:
-            Image.open("static/img/categories/no-img.jpg").save(f"static/img/categories/{last_id}.jpg")
-        category.image = f"img/categories/{last_id}.jpg"
-        db_sess.add(category)
-        db_sess.commit()
-        return redirect("/")
-    return render_template("create_category.html", title=title, form=form, message="")
 
 
 if __name__ == "__main__":
